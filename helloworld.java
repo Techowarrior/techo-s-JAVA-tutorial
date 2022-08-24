5be10c220b395b68f3449149c2a905a622978151
Techowarrior <97187772+Techowarrior@users.noreply.github.com>
2022-08-24 11:26:30 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+// this is class .this is also known as blue print of code.here public means it can be access by anyone. 
+public class helloworld {
+	
+	public static void main(String [] args){
+		
+		// to print a line System.out.println() is used in java;
+		
+		//Example
+		
+		System.out.println("hello world");
+		
+	}
+	
+}
